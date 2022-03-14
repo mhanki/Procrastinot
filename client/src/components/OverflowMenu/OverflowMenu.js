4ff54606328741aa7684e25f673214376e5bab9c
mhanki <meike.hankewicz@protonmail.com>
2022-03-14 16:09:33 +0100

    Add OverflowMenu component

---
+++ client/src/components/OverflowMenu/OverflowMenu.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { 
+  Dropdown, 
+  DropdownMenu, 
+  DropdownToggle, 
+  DropdownItem 
+} from 'reactstrap';
+
+const OverflowMenu = ({items, size=''}) => {
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
+  return(
+    <Dropdown className="overflow-menu" direction="left" isOpen={dropdownOpen} toggle={toggle} options={items}>
+      <DropdownToggle
+        data-toggle="dropdown"
+        tag="span"
+      >
+        <div className={"overflow-menu-icon " + size}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </DropdownToggle>
+      <DropdownMenu>
+        {items.map(item => (
+          <DropdownItem onClick={item.action} key={item.name}>
+            {item.name}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+export default OverflowMenu;
