281b382c84a5b36d60a6df3d9f704e6dd8940ba3
mhanki <meike.hankewicz@protonmail.com>
2022-04-24 09:27:14 +0200

    Add Sentry

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const User = require('./models/users');
+const Sentry = require("@sentry/node");
 
 const userRoutes = require('./routes/users/users');
 const projectRoutes = require('./routes/projects/projects');
@@ -10,7 +11,13 @@ const membersRoutes = require('./routes/members/members');
 const tasksRoutes = require('./routes/tasks/tasks');
 const commentsRoutes = require('./routes/comments/comments');
 
+require('dotenv').config();
+
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN
+});
 
 const sessionConfig = {
   secret: 'secret',
@@ -23,6 +30,7 @@ const sessionConfig = {
   }
 }
 
+app.use(Sentry.Handlers.requestHandler());
 app.use(express.json());
 app.use(session(sessionConfig))
 app.use(passport.initialize());
@@ -37,11 +45,23 @@ app.use((req, res, next) => {
   next();
 })
 
-app.use('/', userRoutes);
+/* app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+}); */
+app.use('/users', userRoutes);
 app.use('/projects', projectRoutes);
 app.use('/tags', tagsRoutes);
 app.use('/members', membersRoutes);
 app.use('/tasks', tasksRoutes);
 app.use('/comments', commentsRoutes);
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Temporary error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 module.exports = app;
